421f850888ca1ea13f8170650af2ecdde27c131d
syorito-hatsuki <enderdron3@gmail.com>
2023-04-13 16:53:55 +0300

    Implemented permission api

--- bmm-fabric/build.gradle.kts
+++ bmm-fabric/build.gradle.kts
@@ -1,10 +1,13 @@
-
 plugins {
     `kotlin-script`
     `core-script`
     `fabric-script`
     `adventure-script`
     `shadow-script`
+}
+
+repositories {
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
@@ -14,4 +17,5 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:0.14.19")
     modImplementation(include("net.kyori:adventure-platform-fabric:5.8.0")!!)
     modImplementation("net.fabricmc.fabric-api:fabric-api:0.77.0+1.19.4")
+    modImplementation(include("me.lucko", "fabric-permissions-api", "0.2-SNAPSHOT"))
 }

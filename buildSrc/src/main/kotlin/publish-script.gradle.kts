e903f701e1ec3f74f9b518fe23704c44afc0392e
Miraculixx <miraculixxyt@gmail.com>
2024-01-25 16:27:11 +0100

    Add auto publishing

---
+++ buildSrc/src/main/kotlin/publish-script.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.modrinth.minotaur")
+}
+
+modrinth {
+    token.set(properties["modrinthToken"] as String)
+    projectId.set(properties["modrinthProjectId"] as? String ?: properties["name"] as String)
+    versionNumber.set(version as String)
+    versionType.set(properties["publishState"] as String)
+
+    // Project sync
+    syncBodyFrom = rootProject.file("README.md").readText()
+}

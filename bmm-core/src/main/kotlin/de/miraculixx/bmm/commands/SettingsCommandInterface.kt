d36c445a2a50a62d5944306dec1f0953ef6a46ae
Miraculixx <miraculixxyt@gmail.com>
2024-06-13 17:36:26 +0200

    Adding 1.0 to 2.0 convertor #28 and bluemap to bmarker convertor

--- bmm-core/src/main/kotlin/de/miraculixx/bmm/commands/SettingsCommandInterface.kt
+++ bmm-core/src/main/kotlin/de/miraculixx/bmm/commands/SettingsCommandInterface.kt
@@ -1,10 +1,21 @@
 package de.miraculixx.bmm.commands
 
+import de.bluecolored.bluemap.api.BlueMapMap
+import de.bluecolored.bluemap.api.gson.MarkerGson
+import de.bluecolored.bluemap.api.markers.*
+import de.miraculixx.bmm.map.MarkerManager
+import de.miraculixx.bmm.map.data.BMarker
+import de.miraculixx.bmm.map.data.BMarkerSet
+import de.miraculixx.bmm.map.data.Box
+import de.miraculixx.bmm.utils.enums.MarkerArg
+import de.miraculixx.bmm.utils.enums.MarkerType
 import de.miraculixx.bmm.utils.locale
-import de.miraculixx.mcommons.text.msg
-import de.miraculixx.mcommons.text.plus
-import de.miraculixx.mcommons.text.prefix
+import de.miraculixx.mcommons.extensions.enumOf
+import de.miraculixx.mcommons.text.*
 import net.kyori.adventure.audience.Audience
+import java.io.File
+import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 interface SettingsCommandInterface {
 
@@ -14,5 +25,160 @@ interface SettingsCommandInterface {
 
     fun sendChangedInfo(sender: Audience, value: String) {
         sender.sendMessage(prefix + locale.msg("command.settings.changed", listOf(value)))
+    }
+
+    fun convertOldMarkers(sender: Audience, configFolder: File) {
+        sender.sendMessage(prefix + cmp("Converting markers from version prior 2.0..."))
+        val oldFolder = File(configFolder, "BlueMap-Marker/marker")
+        if (!oldFolder.exists()) {
+            sender.sendMessage(prefix + cmp("No old markers found!", cError))
+            return
+        }
+        val api = MarkerManager.blueMapAPI
+        if (api == null) {
+            sender.sendMessage(prefix + cmp("The converter only works after BlueMap loads!", cError))
+            return
+        }
+        val gson = MarkerGson.INSTANCE
+
+        oldFolder.listFiles()?.forEach { file ->
+            if (file.extension != "json") return@forEach
+            val name = file.nameWithoutExtension
+            if (name == "marker-sets") return@forEach
+            sender.sendMessage(prefix + cmp("Starting conversion of '$name'..."))
+            val split = name.split('_', limit = 2)
+            val setID = split.first()
+            val mapName = split.getOrNull(1) ?: return@forEach // Invalid file
+            val map = api.maps.firstOrNull { it.name == mapName || it.id == mapName }
+            if (map == null) {
+                sender.sendMessage(prefix + cmp(" - Map '$mapName' not found! Can not convert the set '$setID'", cError))
+                return@forEach
+            }
+
+            val set = kotlin.runCatching {
+                gson.fromJson(file.readText(), MarkerSet::class.java)
+            }.onFailure {
+                sender.sendMessage(prefix + cmp(" - Set '$setID' has an invalid json structure! Skipping it...", cError))
+                sender.sendMessage(prefix + cmp(" - Error: ${it.message}", cError))
+            }.getOrNull() ?: return
+
+            integrateSet(set, map, setID)
+            sender.sendMessage(prefix + cmp(" - Set '$setID' converted successfully! (${set.markers.size} markers)"))
+        }
+        sender.sendMessage(prefix + cmp("Conversion finished!", cSuccess))
+    }
+
+    fun convertIntegratedMarkers(sender: Audience, configFolder: File, mapID: String) {
+        val mapFile = File(configFolder, "BlueMap/maps/$mapID.conf")
+        if (!mapFile.exists()) {
+            sender.sendMessage(prefix + cmp("BlueMap file for '$mapID' not found! ", cError) + cmp("(${mapFile.path})"))
+            return
+        }
+        val api = MarkerManager.blueMapAPI
+        if (api == null) {
+            sender.sendMessage(prefix + cmp("The converter only works after BlueMap loads!", cError))
+            return
+        }
+        val map = api.getMap(mapID).getOrNull()
+        if (map == null) {
+            sender.sendMessage(prefix + cmp("Map '$mapID' not found! Can not convert the markers!", cError))
+            return
+        }
+
+        // Config Hackery
+        val lines = mapFile.readLines().toMutableList()
+        var fullMarkerContent = ""
+        var keywordFound = false
+        lines.indices.forEach { i ->
+            if (lines[i].contains("marker-sets: {")) keywordFound = true
+            if (keywordFound) {
+                val content = lines[i]
+                lines[i] = "#$content"
+                fullMarkerContent += content
+            }
+        }
+        mapFile.writeText(lines.joinToString("\n"))
+
+        // Loading actual content
+        map.markerSets.forEach { (setID, set) ->
+            if (MarkerManager.blueMapMaps[mapID]?.containsKey(setID) == true) return@forEach // BMM marker set
+            if (!fullMarkerContent.contains("$mapID:")) return@forEach // Very soft check if the set is in the file to avoid another addon set conversion
+            sender.sendMessage(prefix + cmp("Starting conversion of '$setID'..."))
+            integrateSet(set, map, setID)
+            sender.sendMessage(prefix + cmp(" - Set '$setID' converted successfully! (${set.markers.size} markers)", cSuccess))
+        }
+        sender.sendMessage(prefix + cmp("Conversion finished! Reloading BlueMap...", cSuccess))
+    }
+
+    private fun integrateSet(set: MarkerSet, map: BlueMapMap, setID: String) {
+        // Load set
+        val bSet = BMarkerSet(UUID(0, 0), set.getArgs(map.id, setID))
+        val finalSet = bSet.load(setID, map)
+
+        // Load Markers
+        set.markers.forEach { (id, marker) ->
+            val bMarker = BMarker(UUID(0, 0), enumOf<MarkerType>(marker.type) ?: MarkerType.POI, marker.getArgs(id))
+            bSet.markers[id] = bMarker
+            bMarker.load(id, finalSet)
+        }
+    }
+
+    private fun MarkerSet.getArgs(mapID: String, setID: String): MutableMap<MarkerArg, Box> {
+        return buildMap {
+            put(MarkerArg.MAP, Box.BoxString(mapID))
+            put(MarkerArg.MARKER_SET, Box.BoxString(setID))
+            put(MarkerArg.LABEL, Box.BoxString(label))
+            put(MarkerArg.TOGGLEABLE, Box.BoxBoolean(isToggleable))
+            put(MarkerArg.DEFAULT_HIDDEN, Box.BoxBoolean(isDefaultHidden))
+            put(MarkerArg.LISTING_POSITION, Box.BoxInt(sorting))
+        }.toMutableMap()
+    }
+
+    private fun Marker.getArgs(id: String): MutableMap<MarkerArg, Box> {
+        return buildMap {
+            put(MarkerArg.ID, Box.BoxString(id))
+            put(MarkerArg.LABEL, Box.BoxString(label))
+            put(MarkerArg.POSITION, Box.BoxVector3d(position))
+            put(MarkerArg.LISTED, Box.BoxBoolean(isListed))
+            put(MarkerArg.LISTING_POSITION, Box.BoxInt(sorting))
+
+            if (this is DetailMarker) put(MarkerArg.DETAIL, Box.BoxString(detail))
+            if (this is DistanceRangedMarker) {
+                put(MarkerArg.MAX_DISTANCE, Box.BoxDouble(maxDistance))
+                put(MarkerArg.MIN_DISTANCE, Box.BoxDouble(minDistance))
+            }
+            if (this is ObjectMarker) {
+                link.getOrNull()?.let { put(MarkerArg.LINK, Box.BoxString(it)) }
+                put(MarkerArg.NEW_TAB, Box.BoxBoolean(isNewTab))
+            }
+            if (this is POIMarker) {
+                put(MarkerArg.ICON, Box.BoxString(iconAddress))
+                put(MarkerArg.ANCHOR, Box.BoxVector2i(anchor))
+            }
+            if (this is LineMarker) {
+                put(MarkerArg.ADD_POSITION, Box.BoxVector3dList(line.points.toMutableList()))
+                put(MarkerArg.DEPTH_TEST, Box.BoxBoolean(isDepthTestEnabled))
+                put(MarkerArg.LINE_WIDTH, Box.BoxInt(lineWidth))
+                put(MarkerArg.LINE_COLOR, Box.BoxColor(lineColor))
+            }
+            if (this is ShapeMarker) {
+                put(MarkerArg.ADD_EDGE, Box.BoxVector2dList(shape.points.toMutableList()))
+                put(MarkerArg.HEIGHT, Box.BoxFloat(shapeY))
+                put(MarkerArg.DEPTH_TEST, Box.BoxBoolean(isDepthTestEnabled))
+                put(MarkerArg.LINE_WIDTH, Box.BoxInt(lineWidth))
+                put(MarkerArg.LINE_COLOR, Box.BoxColor(lineColor))
+                put(MarkerArg.FILL_COLOR, Box.BoxColor(fillColor))
+            }
+            if (this is ExtrudeMarker) {
+                put(MarkerArg.ADD_EDGE, Box.BoxVector2dList(shape.points.toMutableList()))
+                put(MarkerArg.HEIGHT, Box.BoxFloat(shapeMinY))
+                put(MarkerArg.MAX_HEIGHT, Box.BoxFloat(shapeMaxY))
+
+                put(MarkerArg.DEPTH_TEST, Box.BoxBoolean(isDepthTestEnabled))
+                put(MarkerArg.LINE_WIDTH, Box.BoxInt(lineWidth))
+                put(MarkerArg.LINE_COLOR, Box.BoxColor(lineColor))
+                put(MarkerArg.FILL_COLOR, Box.BoxColor(fillColor))
+            }
+        }.toMutableMap()
     }
 }
--- bmm-paper/src/main/kotlin/de/miraculixx/bmm/commands/SettingsCommand.kt
+++ bmm-paper/src/main/kotlin/de/miraculixx/bmm/commands/SettingsCommand.kt
@@ -1,13 +1,16 @@
 package de.miraculixx.bmm.commands
 
+import de.miraculixx.bmm.anyExecutorAsync
+import de.miraculixx.bmm.map.MarkerManager
 import de.miraculixx.bmm.utils.data.manageSettings
 import de.miraculixx.bmm.utils.data.settingsCommandPrefix
 import de.miraculixx.bmm.utils.settings
+import de.miraculixx.kpaper.extensions.bukkit.dispatchCommand
+import de.miraculixx.kpaper.extensions.console
 import dev.jorel.commandapi.CommandTree
-import dev.jorel.commandapi.kotlindsl.anyExecutor
-import dev.jorel.commandapi.kotlindsl.commandTree
-import dev.jorel.commandapi.kotlindsl.integerArgument
-import dev.jorel.commandapi.kotlindsl.literalArgument
+import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.kotlindsl.*
+import java.io.File
 
 class SettingsCommand: SettingsCommandInterface {
     private val settingsCommand = commandTree(settingsCommandPrefix) {
@@ -16,6 +19,21 @@ class SettingsCommand: SettingsCommandInterface {
         // TODO
         literalArgument("language") {}
 
+        literalArgument("convert") {
+            literalArgument("oldBMarkers") {
+                anyExecutorAsync { sender, _ -> convertOldMarkers(sender, File("plugins")) }
+            }
+            literalArgument("bluemapMarkers") {
+                stringArgument("map") {
+                    replaceSuggestions(ArgumentSuggestions.stringCollection { MarkerManager.blueMapMaps.keys })
+                    anyExecutorAsync { sender, args ->
+                        convertIntegratedMarkers(sender, File("plugins"), args[0] as String)
+                        console.dispatchCommand("bluemap reload")
+                    }
+                }
+            }
+        }
+
         intSetting("maxUserSets", { settings.maxUserSets }) { settings.maxUserSets = it }
         intSetting("maxUserMarker", { settings.maxUserMarker }) { settings.maxUserMarker = it }
     }
@@ -23,7 +41,7 @@ class SettingsCommand: SettingsCommandInterface {
     private fun CommandTree.intSetting(name: String, get: () -> Int, set: (Int) -> Unit) = literalArgument(name) {
         anyExecutor { sender, _ -> sendCurrentInfo(sender, get().toString()) }
         integerArgument("new-value", -1) {
-            anyExecutor { sender, args ->
+            anyExecutorAsync { sender, args ->
                 set(args[0] as Int)
                 sendChangedInfo(sender, get().toString())
             }

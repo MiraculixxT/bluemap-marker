4d098bdbc4cdc136597f436e037f311d30885a24
Miraculixx <miraculixxyt@gmail.com>
2024-10-08 18:32:51 +0200

    Adding teleportation message and sound #41

--- bmm-core/src/main/kotlin/de/miraculixx/bmm/commands/TeleportCommandInstance.kt
+++ bmm-core/src/main/kotlin/de/miraculixx/bmm/commands/TeleportCommandInstance.kt
@@ -6,6 +6,8 @@ import de.miraculixx.bmm.utils.enums.MarkerArg
 import de.miraculixx.bmm.utils.locale
 import de.miraculixx.mcommons.text.*
 import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
 import java.util.UUID
 
 interface TeleportCommandInstance {
@@ -19,10 +21,13 @@ interface TeleportCommandInstance {
 
         maps.forEach { map ->
             map.forEach { (_, set) ->
-                set.markers.forEach { (_, marker) ->
+                set.markers.forEach { (id, marker) ->
+                    if (markerID != id) return@forEach
                     val isBypass = isBypass
                     val isTeleporter = marker.attributes[MarkerArg.TELEPORTER]?.getBoolean() == true
                     if (isTeleporter || isBypass || marker.owner == owner) {
+                        sendMessage(prefix + locale.msg("command.teleportMarker", listOf(markerID)))
+                        playSound(Sound.sound(Key.key("entity.enderman.teleport"), Sound.Source.MASTER, 0.6f, 1.1f))
                         return marker.attributes[MarkerArg.POSITION]?.getVector3d()
                     }
                 }
